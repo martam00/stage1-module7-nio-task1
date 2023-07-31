7035395f309292e4725f68dfc3e3074808f6653c
martam00 <marta.michniacka1@gmail.com>
2023-07-31 19:00:57 +0200

    java NIO

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,38 @@
 package com.epam.mjc.nio;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Map<String, String> data = new HashMap<>();
+        try (RandomAccessFile aFile = new RandomAccessFile(file.getAbsolutePath(), "r");
+             FileChannel inChannel = aFile.getChannel()){
+            Path path = Paths.get(file.getAbsolutePath());
+            List<String> text = Files.readAllLines(path);
+
+            for (String s : text) {
+                String[] line = s.split(": ");
+                data.put(line[0], line[1]);
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        String name = data.get("Name");
+        Integer age = Integer.parseInt(data.get("Age"));
+        String email = data.get("Email");
+        Long phone = Long.parseLong(data.get("Phone"));
+
+        return new Profile(name, age, email, phone);
     }
 }
